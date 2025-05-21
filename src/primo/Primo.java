c7eb5448722377d96f391709c8ab9aafa1146ec3
Marina Torre Duran <Marina Torre Duran@LAPTOP-7AN9ONCM>
2025-05-21 11:31:56 +0200

    Master creada

---
+++ src/primo/Primo.java
@@ -0,0 +1,18 @@
+package primo;
+
+import java.util.Scanner;
+
+public class Primo {
+
+	public static void main(String[] args) {
+		Scanner ent = new Scanner(System.in);
+		int numero = 0;
+		
+		System.out.println("Buenos días Ceinmark, soy Marina Torre");
+		
+		System.out.println("Introduce un número para averiguar si es primo o no:");
+		numero = Integer.parseInt(ent.nextLine());
+        
+	}//fin main
+
+}//fin clase
